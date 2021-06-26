cb4e2dad1a04ba06178e53c7cbd63b8f96f6481a
mrezkys <mrezkysulihin@gmail.com>
2021-06-26 19:57:12 +0800

    delete : search feature

--- lib/view/screen/home page/wishlist_page.dart
+++ lib/view/screen/home page/wishlist_page.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kepengen/model/helper/value_checker.dart';
 import 'package:kepengen/provider/local_database.dart';
+import 'package:kepengen/provider/wishlist_provider.dart';
 import 'package:kepengen/view/modal/wishlist_page_filter_modal.dart';
 import 'package:kepengen/view/screen/wishlist_item_detail_page.dart';
 import 'package:kepengen/view/utils/gradient_background.dart';
 import 'package:kepengen/view/widget/main_app_bar.dart';
 import 'package:kepengen/view/widget/skeleton_container.dart';
 import 'package:kepengen/view/widget/wishlist_item_tile.dart';
+import 'package:provider/provider.dart';
 
 class WishlistPage extends StatefulWidget {
   @override
@@ -27,25 +29,6 @@ class _WishlistPageState extends State<WishlistPage> {
   getListWishlist({String filter}) async {
     var _listWishlist = await DBProvider.db.getAllWishlist(filter: filter);
     return _listWishlist;
-  }
-
-  filterSearch(String query) async {
-    var dummySearchlist = await getListWishlist();
-    if (query.isNotEmpty) {
-      var dummyListData = [];
-      dummySearchlist.forEach((item) {
-        if (item.name.toLowerCase().contains(query.toLowerCase())) {
-          dummyListData.add(item);
-        }
-      });
-      setState(() {
-        listWishlist = Future.value(dummyListData);
-      });
-      print(dummyListData);
-      return;
-    } else {
-      listWishlist = getListWishlist();
-    }
   }
 
   @override
@@ -74,63 +57,34 @@ class _WishlistPageState extends State<WishlistPage> {
                   },
                 ),
                 // Search and filter box
-                Container(
-                  color: Colors.transparent,
-                  padding: EdgeInsets.only(left: 30, right: 30, top: 30),
-                  height: 200.0 - 60.0, // 200 = Parent Container Height | 60 = App bar height
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.only(left: 15),
-                        width: MediaQuery.of(context).size.width - 30 - 30 - 50 - 15, // 30 = parent container padding | 15 = space | 50 = filter box
-                        height: 50,
-                        decoration: BoxDecoration(color: Colors.white.withOpacity(0.15), borderRadius: BorderRadius.circular(5)),
-                        child: TextField(
-                          controller: _searchController,
-                          onChanged: (value) {
-                            setState(() {
-                              filterSearch(value);
-                            });
-                          },
+                GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                        elevation: 0.0,
+                        isDismissible: true,
+                        backgroundColor: Colors.transparent,
+                        isScrollControlled: true,
+                        context: context,
+                        builder: (context) {
+                          return WishlistPageFilterModal();
+                        });
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(left: 30, right: 30, top: 25),
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    decoration: BoxDecoration(color: Colors.white.withOpacity(0.22), borderRadius: BorderRadius.circular(5)),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset('assets/icons/Filter.svg', color: Colors.white),
+                        SizedBox(width: 20),
+                        Text(
+                          'Filter Wishlist',
                           style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Cari Wishlist...',
-                            hintStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
-                            icon: SvgPicture.asset(
-                              'assets/icons/Search.svg',
-                              color: Colors.white,
-                            ),
-                          ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          showModalBottomSheet(
-                              elevation: 0.0,
-                              isDismissible: true,
-                              backgroundColor: Colors.transparent,
-                              isScrollControlled: true,
-                              context: context,
-                              builder: (context) {
-                                return WishlistPageFilterModal();
-                              });
-                        },
-                        child: Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(color: Colors.white.withOpacity(0.3), borderRadius: BorderRadius.circular(5)),
-                          child: Center(
-                            child: SvgPicture.asset('assets/icons/Filter.svg', color: Colors.white),
-                          ),
-                        ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -138,70 +92,88 @@ class _WishlistPageState extends State<WishlistPage> {
           ),
           Container(
             margin: EdgeInsets.only(top: 30, bottom: 120),
-            child: FutureBuilder(
-              future: listWishlist,
-              builder: (_, data) {
-                print(data.data);
-                // TODO: fix error because of no error handler if return data is empty
-                if (data.connectionState == ConnectionState.done) {
-                  if (!ValueChecker.isNullOrEmpty(data.data)) {
-                    return ListView.separated(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: data.data.length,
-                      separatorBuilder: (context, index) {
-                        return SizedBox(height: 15);
-                      },
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          child: WishlistItemTile(
-                            index: index,
-                            data: data.data[index],
-                          ),
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => WishlistItemDetailPage(
-                                  index: index,
-                                ),
+            child: Consumer<WishlistProvider>(
+              builder: (context, _wishlistProvider, _) {
+                if (_wishlistProvider.isCompleted == true) {
+                  listWishlist = getListWishlist(filter: 'completed');
+                } else if (_wishlistProvider.isInCompleted == true) {
+                  listWishlist = getListWishlist(filter: 'incompleted');
+                } else if (_wishlistProvider.filterTerdekat == true) {
+                  listWishlist = getListWishlist(filter: 'terdekat');
+                } else if (_wishlistProvider.filterTerpengen == true) {
+                  listWishlist = getListWishlist(filter: 'terpengen');
+                } else if (_wishlistProvider.filterTermurah == true) {
+                  listWishlist = getListWishlist(filter: 'termurah');
+                } else if (_wishlistProvider.filterTermahal == true) {
+                  listWishlist = getListWishlist(filter: 'termahal');
+                } else {
+                  listWishlist = getListWishlist(filter: 'all');
+                }
+                return FutureBuilder(
+                  future: listWishlist,
+                  builder: (_, data) {
+                    print(data.data);
+                    if (data.connectionState == ConnectionState.done) {
+                      if (data.data.isNotEmpty) {
+                        return ListView.separated(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: data.data.length,
+                          separatorBuilder: (context, index) {
+                            return SizedBox(height: 15);
+                          },
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              child: WishlistItemTile(
+                                index: index,
+                                data: data.data[index],
                               ),
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => WishlistItemDetailPage(
+                                      index: index,
+                                    ),
+                                  ),
+                                );
+                              },
                             );
                           },
                         );
-                      },
-                    );
-                  } else {
-                    return Container(
-                      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.width - 60,
-                      color: Colors.white,
-                      child: SvgPicture.asset('assets/images/no-data.svg'),
-                    );
-                  }
-                } else {
-                  return ListView.separated(
-                    padding: EdgeInsets.only(bottom: 100),
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: 3,
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        height: 10,
-                      );
-                    },
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: SkeletonContainer.square(
-                          height: 80,
+                      } else {
+                        return Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                           width: MediaQuery.of(context).size.width,
-                          borderRadius: 10,
-                        ),
+                          height: MediaQuery.of(context).size.width - 60,
+                          color: Colors.white,
+                          child: SvgPicture.asset('assets/images/no-data.svg'),
+                        );
+                      }
+                    } else {
+                      return ListView.separated(
+                        padding: EdgeInsets.only(bottom: 100),
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: 3,
+                        separatorBuilder: (context, index) {
+                          return SizedBox(
+                            height: 10,
+                          );
+                        },
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 30),
+                            child: SkeletonContainer.square(
+                              height: 80,
+                              width: MediaQuery.of(context).size.width,
+                              borderRadius: 10,
+                            ),
+                          );
+                        },
                       );
-                    },
-                  );
-                }
+                    }
+                  },
+                );
               },
             ),
           ),
